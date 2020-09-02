5f309b796e7a1221301718532ac76ced96a73cb4
PlayerX-SyntaX <70642783+PlayerX-SyntaX@users.noreply.github.com>
2020-09-02 20:52:27 +0700

    Add files via upload

---
+++ Start.sh
@@ -0,0 +1,87 @@
+#!/system/bin/sh
+
+echo $Green
+echo "This Script Is Written By Sniff Kamsuy"
+sleep 1
+echo "--------------------------------------"
+sleep 1
+echo " Copyright Stephin "
+sleep 1
+echo "-------------------"
+sleep 2
+echo "."
+sleep 1
+echo "We Are Pentester"
+sleep 1
+echo "My Team PLAYER - X"
+sleep 1
+echo "------------------"
+sleep 2
+echo "."
+sleep 1
+apt-get update -y
+#Updating .....
+apt install python
+#Installation ...... 
+apt install python2 ruby bash nano neofetch screenfetch figlet git cowsay toilet nodejs -y
+gem install lolcat
+pip install requests flask colorama bs4
+pip2 install requests flask colorama bs4
+sleep 2
+echo "--------------------------------------------------"
+sleep 1
+echo "you can now hack anything"
+sleep 1
+echo "completing wait ... "
+sleep 5
+echo "3 ..."
+sleep 4
+echo "2 ..."
+sleep 3
+echo "1 ..."
+sleep 2
+echo "Starting !!!"
+sleep 1
+echo "##################################################"
+apt install php perl nmap openssh -y
+sleep 1
+echo "##################################################"
+apt install clang jq macchanger -y
+sleep 1
+echo "##################################################"
+sleep 1.5
+echo "Anonymous"
+sleel 1.5
+echo "Join Us On Facebook {Anonymous Tim 3}"
+sleep 1
+echo "##################################################"
+apt install curl zip tar fish tor wget w3m cmatrix -y
+echo "##################################################"
+echo "You're Subscribe Is Support My Team"
+sleep 1
+echo "##################################################"
+apt install wcalc openssl bmon -y
+echo "##################################################"
+sleep 1
+echo " "
+echo "processing wait ...... "
+sleep 3
+echo "##################################################"
+sleep 2
+echo "If You Connected To Internet Then You Downloaded Success Fully Subscribe Us Anonymous Tim 3 & PLAYER - X"
+echo " "
+sleep 3
+echo "We Are Anonymous"
+sleep 1
+echo "We Are Legion"
+sleep 1
+echo "We Do Not Forgive"
+sleep 1
+echo "We Do Not Forget"
+sleep 1
+echo "Expect Us !!!"
+sleep 5
+echo " "
+echo "Now Your Terminal Is Ready To Use !!!"
+
+exit
